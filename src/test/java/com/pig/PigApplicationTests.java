90d75732ff740c5f2bf121a874c067cf9bec11f4
chanhong-dev <zxck03051@gmail.com>
2022-10-21 15:49:11 +0900

    [add] project

---
+++ src/test/java/com/pig/PigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
