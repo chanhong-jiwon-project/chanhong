a89585fc2000454e880487849872247ce80a29d9
chanhong-dev <zxck03051@gmail.com>
2022-10-25 17:56:59 +0900

    [add] security-setting - (#5)

--- src/main/java/com/pig/config/WebSecurityConfig.java
+++ src/main/java/com/pig/config/WebSecurityConfig.java
@@ -10,7 +10,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +27,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
+
+        httpSecurity
+//                .authorizeRequests()
+//                // image 폴더를 login 없이 허용
+//                .antMatchers("/image/**").permitAll()
+//                // css 폴더를 login 없이 허용
+//                .antMatchers("/css/**").permitAll()
+//                .antMatchers("/userInfo/**").permitAll()
+//                .antMatchers("/h2-console/**").permitAll()
+//                .antMatchers("/sign-in").permitAll()
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/js/**.js").permitAll()
+//                .antMatchers("/signup").permitAll()
+//                .antMatchers("/static/**.html").permitAll()
+//                .antMatchers("/**.html").permitAll()
+//                .antMatchers("/users").permitAll()
+//                .antMatchers("/admin/**").permitAll()
+//                .antMatchers("/roles").permitAll()
+//                // 그 외 모든 요청은 인증과정 필요
+//                .anyRequest().authenticated()
+//                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
